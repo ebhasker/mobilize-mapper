d64a8715bf7b52db2a6517d081b628edad0993cc
Ezekiel Bhasker <ezekiel.bhasker@gmail.com>
2021-06-24 18:08:27 -0700

    add leaflet, better event cards, more unit tests

--- html/app.js
+++ html/app.js
@@ -118,7 +118,6 @@ const [MobilizeMapperApp, runMobilizeMapperAppTests] = (function() {
     const bulmaLayoutManager = {
         block: (...content) => e("div", {className: "block"}, ...content),
         box: (...content) => e("div", {className: "box"}, ...content),
-        paragraph: (...content) => e("p", {}, ...content),
         card: ({title, imageSrc, imageHeight, content}) => e("div", {className: "card"},
             e("div", {className: "card-image"},
                 imageSrc ? e("figure", {
@@ -130,7 +129,7 @@ const [MobilizeMapperApp, runMobilizeMapperAppTests] = (function() {
             e("header", {className: "card-header"},
                 e("p", {className: "card-header-title"}, title)
             ),
-            ...content
+            e("div", {className: "card-content"}, ...content)
         ),
         title: (text) => e("h1", {className: "title is-4"}, text),
         message: ({title, content}) => e("article", {className: "message"},
@@ -138,8 +137,9 @@ const [MobilizeMapperApp, runMobilizeMapperAppTests] = (function() {
             e("div", {className: "message-body"}, ...content)
         ),
         columns: (...content) => e("div", {className: "columns"}, ...content),
-        column: ({classes, content}) =>
+        column: ({classes, content, style}) =>
             e("div", {
+                style: style,
                 className: "column" + (classes? " " + classes.join(" ") : "")
             }, ...content),
         pagination: ({currentPage, unorderedLinks, orderedLinks}) =>
@@ -226,32 +226,105 @@ const [MobilizeMapperApp, runMobilizeMapperAppTests] = (function() {
         }
 
         // finished loading events, initialize view
-        const ui = ({box, block, columns, column, title}) => box(
-            block(title("Mobilize Events")),
-            block(
-                PaginationView({
-                    currentPage: state.events.page,
-                    pageCount: state.events.pageCount,
-                    enabled: state.events.loaded,
-                    setPage: (page) => actions.setEventsPage(page)
-                })
+        const ui = ({box, block, columns, column, title}) => e("div", {style:{height: "100vh"}},
+            e("div", {className: "box", style: {}},
+                block(title("Mobilize Events")),
+                block(
+                    PaginationView({
+                        currentPage: state.events.page,
+                        pageCount: state.events.pageCount,
+                        enabled: state.events.loaded,
+                        setPage: (page) => actions.setEventsPage(page)
+                    })
+                )
             ),
-            columns(
-                column({
-                    classes: ["is-one-quarter"],
-                    content: [eventListUi]
-                }),
-                column({
-                    classes: ["is-three-quarters"],
-                    content: [
-                        block(title("Map")),
-                    ]
-                })
+            e("div", {},
+                columns(
+                    column({
+                        classes: ["is-one-third"],
+                        style: {height: "85vh", overflowY: "scroll", wordBreak: "break-word"},
+                        content: [eventListUi]
+                    }),
+                    column({
+                        classes: ["is-two-thirds"],
+                        content: [e(MapView, {info: state.events.info})]
+                    })
+                )
             )
         );
 
         return render(ui);
     };
+    
+    function MapView({info}) {
+
+        const mapRef = React.useRef(null);
+
+        const tileRef = React.useRef(null);
+
+        const markerLayerRef = React.useRef([]);
+
+        // Open Street Maps Tiler
+        tileRef.current = L.tileLayer(
+            `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`,
+            {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }
+        );
+
+        // initialize map and controls
+        React.useEffect(() => {
+            mapRef.current = L.map("mapper", {
+                center: [37, -95], // initially: roughly the USA
+                zoom: 3,
+                zoomControl: false,
+                //maxBounds: L.latLngBounds(L.latLng(-150, -240), L.latLng(150, 240)),
+                layers: [tileRef.current]
+            });
+
+            // zoom control on top-right instead of top-left
+            L.control.zoom({position: "topright"}).addTo(mapRef.current);
+            
+            // initialize a layer for our markers
+            markerLayerRef.current = L.layerGroup().addTo(mapRef.current);
+        }, []);
+
+        // Add markers:
+        React.useEffect(() => {
+            const markers = info !== null ? info.data.reduce(
+                function(result, {location, title, venue}) {
+                    if(
+                        location && location.location &&
+                        location.location.latitude && location.location.longitude) {
+
+                        result.push({
+                            latitude: location.location.latitude,
+                            longitude: location.location.longitude,
+                            title: title,
+                            venue: location.venue
+                        });
+                    }
+                    return result;
+                }, []) : [];
+
+            markerLayerRef.current.clearLayers();
+
+            markers.forEach(({latitude, longitude, title, venue}) => {
+                const marker = L.marker(new L.LatLng(latitude, longitude)).addTo(markerLayerRef.current);
+                marker.bindPopup(`<b>${title}</b><br>${venue}`);
+            });
+            //console.log(mapRef.current);
+            //mapRef.current.panTo(new L.LatLng(0, 0));
+        }, [info]);
+
+        //console.log(markers);
+        const ui = ({block}) => e("div",
+            {
+                id: "mapper",
+                style: {height: "100%", width: "100%", overflow: "hidden"}
+        });
+        return render(ui);
+    }
 
     function PaginationView({currentPage, pageCount, enabled, setPage}) {
         const makeLink = (props, text) => e("a", {...props, disabled: !enabled}, text);
@@ -309,7 +382,7 @@ const [MobilizeMapperApp, runMobilizeMapperAppTests] = (function() {
         //  - previous: link to next if applicable, or null
         //  - data: the array of events
         //  - metadata: misc. stuff like build, etc. - doesn't seem too useful at the moment
-        const ui = ({block}) => block(
+        const ui = ({block}) => e("div", {className: "block is-black"},
             // debug: look at raw data
             //e("pre", {}, JSON.stringify(info, null, 2)),
             ...info.data.map((entry) => e(EventCard, {key: entry.id.toString(), entry: entry}))
@@ -318,24 +391,61 @@ const [MobilizeMapperApp, runMobilizeMapperAppTests] = (function() {
         return render(ui);
     }
 
+    function LocationView(details) {
+        const {
+            venue,
+            address_lines,
+            locality,
+            region,
+            country,
+            postal_code,
+            location
+        } = details;
+
+
+        const ui = ({block}) => e("div",
+            {className: "box message is-clickable",
+                onClick: () => {
+                    // TBD - center on appropriate marker
+                }
+            },
+            ...[]
+                .concat(venue? [
+                    block(
+                        e("strong", {}, "Venue"),
+                        e("p", {}, venue)
+                    )] : [])
+                .concat(address_lines ? [
+                    block(
+                        e("strong", {}, "Address"),
+                        e("p", {}, address_lines.filter((elem) => elem).join(", ")),
+                        e("p", {}, [
+                            locality,
+                            region,
+                            country,
+                            postal_code].filter((elem) => elem).join(", "))
+                    )] : []
+                )
+        );
+        //e("pre", {}, JSON.stringify(entry, null, 2)));
+        return render(ui);
+    }
+
     function EventCard({entry}) {
         // event has:
         //  - TBD
-        const ui = ({card, message, paragraph}) => 
+        const ui = ({box, block, card, message}) => block(
             card({
                 title: entry.title,
                 imageSrc: entry.featured_image_url,
                 imageHeight: "15rem",
-                content: [
-                    message({
-                        title: "Description",
-                        content: (entry.summary? [paragraph(e("strong", {}, entry.summary)), paragraph(entry.description)] : [
-                            paragraph(entry.description)
-                        ])
-                    }),
-                    //e("pre", {}, JSON.stringify(entry, null, 2))
-                ]
-            });
+                content:(entry.location ? [e(LocationView, entry.location)] : []
+                    ).concat(entry.description? [
+                        e("strong", {}, "Description"),
+                        e("p", {}, entry.description)
+                    ] : [])
+                
+            }));
         
         return render(ui);
     }
@@ -351,7 +461,7 @@ const [MobilizeMapperApp, runMobilizeMapperAppTests] = (function() {
             return {
                 test: function(name, callback) {
                     cases.forEach((params) => {
-                        QUnit.test(name + "(" + JSON.stringify(params) + ")", (assert) => callback(params, assert))
+                        QUnit.test(name + " (" + JSON.stringify(params) + ")", (assert) => callback(params, assert))
                     });
                 }
             }
@@ -359,12 +469,98 @@ const [MobilizeMapperApp, runMobilizeMapperAppTests] = (function() {
         
         QUnit.module('init', function() {
             QUnit.cases([1,33,1241].map((x) => ({organizationId: x})))
-                .test('should result in correct state initialization', function(params, assert) {
+                .test('should result in correct state initialization ', function(params, assert) {
                     state = init(params);
                     assert.equal(state.organizationId, params.organizationId);
                     assert.equal(state.events.loaded, false);
                 });
         });
+        
+        QUnit.module('computePageList', function() {
+            QUnit.cases([
+                {
+                    "name": "page 1 of 0 total",
+                    "args": [1, 0],
+                    "expected": [1]
+                },
+                {
+                    "name": "page 1 of 0 total",
+                    "args": [1, 1],
+                    "expected": [1]
+                },
+                {
+                    "name": "page 1 of 193 total",
+                    "args": [1, 193],
+                    "expected": [1, 2, 3, null, 193]
+                },
+                {
+                    "name": "page 2 of 193 total",
+                    "args": [2, 193],
+                    "expected": [1, 2, 3, 4, null, 193]
+                },
+                {
+                    "name": "page 3 of 193 total",
+                    "args": [3, 193],
+                    "expected": [1, 2, 3, 4, 5, null, 193]
+                },
+                {
+                    "name": "page 4 of 193 total",
+                    "args": [4, 193],
+                    "expected": [1, 2, 3, 4, 5, 6, null, 193]
+                },
+                {
+                    "name": "page 5 of 193 total",
+                    "args": [5, 193],
+                    "expected": [1, 2, 3, 4, 5, 6, 7, null, 193]
+                },
+                {
+                    "name": "page 6 of 193 total",
+                    "args": [6, 193],
+                    "expected": [1, null, 4, 5, 6, 7, 8, null, 193]
+                },
+                {
+                    "name": "page 70 of 193 total",
+                    "args": [70, 193],
+                    "expected": [1, null, 68, 69, 70, 71, 72, null, 193]
+                },
+                {
+                    "name": "page 188 of 193 total",
+                    "args": [188, 193],
+                    "expected": [1, null, 186, 187, 188, 189, 190, null, 193]
+                },
+                {
+                    "name": "page 189 of 193 total",
+                    "args": [189, 193],
+                    "expected": [1, null, 187, 188, 189, 190, 191, 192, 193]
+                },
+                {
+                    "name": "page 190 of 193 total",
+                    "args": [190, 193],
+                    "expected": [1, null, 188, 189, 190, 191, 192, 193]
+                },
+                {
+                    "name": "page 191 of 193 total",
+                    "args": [191, 193],
+                    "expected": [1, null, 189, 190, 191, 192, 193]
+                },
+                {
+                    "name": "page 192 of 193 total",
+                    "args": [192, 193],
+                    "expected": [1, null, 190, 191, 192, 193]
+                },
+                {
+                    "name": "page 193 of 193 total",
+                    "args": [193, 193],
+                    "expected": [1, null, 191, 192, 193]
+                },
+            ])
+                .test('should match', function(params, assert) {
+                    result = computePageList(...params.args);
+                    assert.deepEqual(result, params.expected);
+                });
+        });
+
+        
     }
 
     return [App, runTests];
